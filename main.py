022b3581df87320616710bc6ceff2a7f683a3f68
mhwahdan <61120913+mhwahdan@users.noreply.github.com>
2021-05-14 13:03:10 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+from google_images_search import GoogleImagesSearch
+
+# using your custom progressbar function
+def my_progressbar(url, progress):
+    print(url + ' ' + progress + '#')
+
+# you can provide API key and CX using arguments,
+# or you can set environment variables: GCS_DEVELOPER_KEY, GCS_CX
+gis = GoogleImagesSearch('AIzaSyCsTj4ORxL98_xtyWN7yyXpxnbwCGdQD-I', '864ad66b1230b9dd5')
+Searchindex = []
+
+
+keywords = open('keywords.txt', 'r')
+
+for x in keywords:
+    Searchindex.append(x.rstrip('\n'))
+
+#enter search indexes here
+
+
+search_params = {
+    'q': '',
+    'num': 300,
+    'fileType': 'jpg',
+}
+
+for index in list(Searchindex):
+    search_params['q'] = index
+    try:
+        gis.search(search_params=search_params, path_to_dir=index,progressbar_fn=my_progressbar)
+        Searchindex.pop(index)
+    except:
+        continue
